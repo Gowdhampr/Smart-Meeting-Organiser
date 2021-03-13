72187daa8527a52a9ed715edd865c98a8cc5d7d3
Gowdham Rajendran <prgowdham@gmail.com>
2021-03-13 15:25:51 +0530

    Default layout added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 
+// Style
+import "./App.css";
+
+/** Layouts **/
+import DefaultLayout from "./containers/DefaultLayout";
+
+// Views
+import Dashboard from "./views/Dashboard";
+
+/*
+   App Content
+*/
 function App() {
   return (
-    <div className="App">
-      {/* App content */}
+    <div>
+      <div className="routeSection">
+        <Router>
+          <Switch>
+            <DefaultLayout
+              exact
+              name="Dashboard"
+              path="/"
+              component={Dashboard}
+            />
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+// Home
+import Dashboard from "./views/Dashboard";
+
+// List of routes
+const routes = [
+  {
+    path: "/",
+    exact: true,
+    name: "Dashboard",
+    component: Dashboard
+  }
+];
+
+export default routes;
